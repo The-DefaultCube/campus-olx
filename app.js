1208a62c105508f64143a46e99c8e79f3470e472
MANISH <71868648+The-DefaultCube@users.noreply.github.com>
2022-06-30 20:00:03 +0530

    added databse create and read operations

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(
 );
 /*----------------------------------------------------------------------------*/
 let port = process.env.PORT;
-if (port === null || port === "") {
+if (port === null || port === "" || port === undefined) {
   port = 3000;
 }
 app.listen(port, function () {
-  console.log("Running on Port : "+port)
+  console.log("Running on Port : " + port);
 });
 /*----------------------------------------------------------------------------*/
 /*-----------------------------DATABASE-------------------------------------*/
@@ -43,14 +43,13 @@ const itemSchema = new mongoose.Schema({
   name: String,
   price: Number,
   description: String,
-  images: [String];
 });
 
-const userSchema = new mongoose.Schema({
-  name: String//Stores full name
-});
+// const userSchema = new mongoose.Schema({
+//   name: String//Stores full name
+// });
 
-const Post = mongoose.model("Post", postSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 /*----------------------------------------------------------------------------*/
 
@@ -58,28 +57,50 @@ const Post = mongoose.model("Post", postSchema);
 /*--------------------------------ROUTES-------------------------------------*/
 
 //Login Page
-app.get("/login", (req, res)=>{
-
-});
+app.get("/login", (req, res) => {});
 
 //HOME PAGE
-app.get("/", (req, res)=>{
-
+app.get("/", (req, res) => {
+  //try to add filter here
+  Item.find({}, (err, foundItems) => {
+    if (!err) {
+      // console.log(typeof(foundItems[0].price))//number
+      res.render("home", { items: foundItems });
+    } else console.log("error retrieving all items");
+  });
 });
 
 //Profile Page
-app.get("/profile", (req,res)=>{
+app.get("/profile", (req, res) => {});
 
-});
-
-//Product Page
-app.get("/product/:productId", (req, res)=>{
-
-});
+//Individual Item Page
+app.get("/item/:itemId", (req, res) => {});
 
 //Selling Page
-app.get("/sell", (req, res)=>{
-
+app.get("/sell", (req, res) => {
+  res.render("sell");
 });
 
+/*--------------------------------------------------------------------------*/
+app.post("/sell", (req, res) => {
+  //create new item
+  // console.log(typeof(req.body.item_price));//string
+  const newItem = new Item({
+    name: req.body.item_name, //string
+    price: req.body.item_price, //number
+    description: req.body.item_description, //string
+  });
 
+  newItem.save((err) => {
+    if (!err) {
+      console.log("Item saved to DB successfully !");
+      res.redirect("/");
+    } else console.log("Error while saving items to DB");
+  });
+});
+
+//css class >> abc-def
+//post req variables >> abc_def
+//js and ejs var >> abcDef
+
+//post request as string only
