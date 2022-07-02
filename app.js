1c576a69128b358bf6f17cc271e70ebec7aaef74
MANISH <71868648+The-DefaultCube@users.noreply.github.com>
2022-07-02 14:49:15 +0530

    added cloudinary image upload feature

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const path = require("path");
 const _ = require("lodash");
+const cloudinary = require("cloudinary").v2;
+const fileUpload = require("express-fileupload");
 /*----------------------------------------------------------------------------*/
 const app = express();
 app.set("view engine", "ejs");
@@ -19,6 +21,11 @@ app.use(express.static("public"));
 app.use(
   bodyParser.urlencoded({
     extended: true,
+  })
+);
+app.use(
+  fileUpload({
+    useTempFiles: true,
   })
 );
 /*----------------------------------------------------------------------------*/
@@ -29,7 +36,13 @@ if (port === null || port === "" || port === undefined) {
 app.listen(port, function () {
   console.log("Running on Port : " + port);
 });
-/*----------------------------------------------------------------------------*/
+/*----------------------------Cloudinary----------------------------------*/
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 /*-----------------------------DATABASE-------------------------------------*/
 // mongoose.connect(
 //   "mongodb+srv://admin-blog-website:" +
@@ -43,6 +56,7 @@ const itemSchema = new mongoose.Schema({
   name: String,
   price: Number,
   description: String,
+  imageUrl: String,
 });
 
 // const userSchema = new mongoose.Schema({
@@ -77,20 +91,15 @@ app.get("/profile", (req, res) => {});
 app.get("/item/:itemId", (req, res) => {
   const itemId = req.params.itemId;
   //find the item
-  Item.findOne({_id: itemId}, (err, foundItem)=>{
-    if(!err)
-    {
-      if(foundItem)
-        res.render("item", {item: foundItem});
-      else
-        res.send("Sorry Item Doesn,t Exist");
+  Item.findOne({ _id: itemId }, (err, foundItem) => {
+    if (!err) {
+      if (foundItem) res.render("item", { item: foundItem });
+      else res.send("Sorry Item Doesn,t Exist");
+    } else {
+      console.log("error retrieving item" + err);
+      res.send("Sorry Item Doesn,t Exist");
     }
-    else
-    {
-      console.log("error retrieving item"+err);
-      res.send("Sorry Item Doesn,t Exist"); 
-    }
-  })
+  });
 });
 
 //Selling Page
@@ -101,19 +110,53 @@ app.get("/sell", (req, res) => {
 /*--------------------------------------------------------------------------*/
 app.post("/sell", (req, res) => {
   //create new item
-  // console.log(typeof(req.body.item_price));//string
-  const newItem = new Item({
-    name: req.body.item_name, //string
-    price: req.body.item_price, //number
-    description: req.body.item_description, //string
-  });
-
-  newItem.save((err) => {
-    if (!err) {
-      console.log("Item saved to DB successfully !");
-      res.redirect("/");
-    } else console.log("Error while saving items to DB");
-  });
+  if (req.files === null) {
+    //save default image url
+    const newItem = new Item({
+      name: req.body.item_name, //string
+      price: req.body.item_price, //number
+      description: req.body.item_description, //string
+      imageUrl: "https://res.cloudinary.com/dwytcg1ux/image/upload/q_30/v1656751976/campusOLX_items/default_item_x6mw8w.jpg", //string
+    });
+    newItem.save((error) => {
+      if (!error) {
+        console.log("Item saved to DB successfully !");
+        res.redirect("/");
+      } else console.log("Error while saving items to DB");
+    });
+  } else {
+    //save to cloudinary
+    const itemImage = req.files.itemImage;
+    const itemImageOptions = {
+      folder: "campusOLX_items",
+      quality: "auto",
+      fetch_format: "auto",
+      width: 800,
+      crop: "scale",
+    };
+    cloudinary.uploader.upload(
+      itemImage.tempFilePath,
+      itemImageOptions,
+      (err, result) => {
+        if (!err) {
+          const newItem = new Item({
+            name: req.body.item_name, //string
+            price: req.body.item_price, //number
+            description: req.body.item_description, //string
+            imageUrl: result.url,
+          });
+          newItem.save((error) => {
+            if (!error) {
+              console.log("Item saved to DB successfully !");
+              res.redirect("/");
+            } else console.log("Error while saving items to DB");
+          });
+        } else {
+          console.log("error saving to cloudinary");
+        }
+      }
+    );
+  }
 });
 
 //css class >> abc-def
